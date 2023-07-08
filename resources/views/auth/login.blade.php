034f2340a3e54ff30eb4ac35f71593a430111251
AlifFajarNR28 <126595005+AlifFajarNR28@users.noreply.github.com>
2023-07-08 22:03:37 +0700

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -22,16 +22,15 @@
     <div class="container">
         <!-- Outer Row -->
         <div class="row justify-content-center">
-            <div class="col-xl-10 col-lg-12 col-md-9">
+            <div class="col-xl-6 col-lg-8 col-md-9">
                 <div class="card o-hidden border-0 shadow-lg my-5">
                     <div class="card-body p-0">
                         <!-- Nested Row within Card Body -->
                         <div class="row">
-                            <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
-                            <div class="col-lg-6">
+                            <div class="col-lg-12">
                                 <div class="p-5">
                                     <div class="text-center">
-                                        <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
+                                        <h1 class="h4 text-gray-900 mb-4">Selamat Datang Kembali Di TAHURA R.SOERJO!</h1>
                                     </div>
                                     <form action="{{ route('login.action') }}" method="POST" class="user">
                                         @csrf
@@ -47,7 +46,7 @@
                                         <div class="form-group">
                                             <input name="email" type="email" class="form-control form-control-user"
                                                 id="exampleInputEmail" aria-describedby="emailHelp"
-                                                placeholder="Enter Email Address...">
+                                                placeholder="Masukkan Alamat Email...">
                                         </div>
                                         <div class="form-group">
                                             <input name="password" type="password"
